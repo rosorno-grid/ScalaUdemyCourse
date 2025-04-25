1944976a72027c2bcf9bb71aac4c26b4487c92d0
Roberto Osorno <rosorno@griddynamics.com>
2025-04-25 09:24:32 -0600

    update

--- src/main/scala/lectures/part1as/DarkSugar.scala
+++ src/main/scala/lectures/part1as/DarkSugar.scala
@@ -1,5 +1,15 @@
 package lectures.part1as
 
 object DarkSugar {
+  //Methods w/single param
+  def singleArgMethod(x: Int): String = s"$x little ducks"
+
+  val descrption = singleArgMethod {
+    val x = 5
+    val y = 6
+    y + x
+  }
+  println(descrption)
+
 
 }
--- src/main/scala/lectures/part1as/Recap.scala
+++ src/main/scala/lectures/part1as/Recap.scala
@@ -1,5 +1,42 @@
 package lectures.part1as
 
-object Recap {
+import scala.annotation.tailrec
 
+object Recap extends App{
+
+  /*
+  Scala is about exxressions
+   */
+  val aConditions: Boolean = false
+  val aConditionsVal = if(aConditions) 42 else 65
+  /*
+  Instructions vs expresions
+  Instruccions: Imperative languages, sequential to do things
+  Expresisions: Buidl expressions on top of other Expressions
+   */
+  val aCodeBlock = {
+    val a = 3
+    val b = 5
+    a + b // aCodeBlock == 8
+  }
+
+  /*
+  UNIT= only do side efects. Equivalent to void
+   */
+  def aFunction(x: Int): Int = aCodeBlock + 1
+  //In order to avoid stackOVerflow use tail recursion
+
+  @tailrec def factorial(n: Int, acc: Int): Int = {
+    if (n <= 0) return acc
+    else factorial (n-1, acc * n)
+  }
+  //Functional programing
+
+  val otherIncrementer = (x: Int) => x +1
+  List(1, 2 ,3).map(otherIncrementer)
+
+  val pairs = for{
+    num <- List(1, 2, 3)
+    str <- List("1","b", "3")
+  } yield num + "-" + str
 }
